37bf38da7f71ac9d8e7a18850438773bcf9ace7f
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-13 19:30:49 -0400

    skeletone code

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+
+package baseline;
+//imports scanner
+
+public class Solution41 {
+    //creates a scanner
+    public static void main(String[] args) {
+        //creates a new initiation of solution
+        //create a new initiation of nameSorter
+        //opens the file
+        //calls the readInput function
+        //calls the sortNames function
+        //calls the writeName function
+        //closes the files
+    }
+}
---
+++ exercise41/src/main/java/baseline/nameSorter.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class nameSorter {
+    //create an instance variable of array list of the names
+
+    public void readNames(String filename){
+        //reads in the file names from the string provided
+        //adds the names to the array list
+    }
+    public void sortNames{
+        //takes the array list and uses sort in the library
+    }
+    public void writeNames(String filename){
+        //takes array list and writes it to a file
+    }
+
+
+}
+
