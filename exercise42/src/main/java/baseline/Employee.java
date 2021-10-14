8b840441212424eb9edd34f5a31d99e78ea9ab9a
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-14 15:12:37 -0400

    skeletone code

---
+++ exercise42/src/main/java/baseline/Employee.java
@@ -0,0 +1,27 @@
+package baseline;
+
+
+import java.util.ArrayList;
+//imports scanner
+//import all the stuff about reading files
+public class Employee {
+    //creates an instance variable of ArrayList that holds all the information
+    
+    public ArrayList<String> readNames(String filename) {
+        //opens input file
+        //reads in the file names from the string provided
+        //adds the names and information  into the array list
+        //returns names for testing purposes
+    }
+    public ArrayList<String> displayEmployees{
+        //takes the array list and is designed to print it
+        //creates a new array list
+        //prints a header
+        // looks through each line
+            //after every word creates spacing
+            //done by parsing through
+            //prints the new modify line
+            //adds new line to new array list
+        //returns the new array list
+    }
+}
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+
+public class Solution42 {
+    public static void main(String[] args) {
+        //creates a new instance of class Employee
+
+        //calls the read file function in Employee with the paramater of the doc file
+
+        //calls the displayEmployees function
+
+    }
+}
