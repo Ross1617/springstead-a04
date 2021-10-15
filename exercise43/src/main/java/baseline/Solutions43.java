b458dae2b12c7142fa9e8fd2b6e27b7cfa6127eb
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-15 17:02:21 -0400

    skeletone code

---
+++ exercise43/src/main/java/baseline/Solutions43.java
@@ -0,0 +1,19 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+
+
+public class Solutions43 {
+    public static void main(String[] args) {
+        //create a new instance of fileMaker
+
+        //calls the function that asks the users all the questions
+        // getUserInformation
+
+        //call the website function creator
+
+
+    }
+}
---
+++ exercise43/src/main/java/baseline/fileMaker.java
@@ -0,0 +1,43 @@
+package baseline;
+
+public class fileMaker {
+    // has 4 instance variables
+    // site name
+    // author
+    //javaScript
+    //CSS
+
+    public void getUserInformation(){
+        //prompt the user for site name
+        //sets the site name to the instance variable
+
+        //prompt the user for the author
+        //sets the author name to the instance variable
+
+        // prompt the user for if he wants JavaScript files
+        //set the instance variable
+
+        //prompt the user if he wants a CSS file
+        //set the instance variable
+    }
+
+    public void createFiles(){
+        //get the current path of the user
+        //assign it to a variable
+        //add the site name to the variable
+        //create a new file for the current path
+        //print out the location
+
+        //write to the file the html needed
+        //make sure it has a title and the author
+        //print the  location
+
+        //check to see if javaScript is equal to y
+            //if yes create a new folder for it
+            //print where it is located
+        //check to see if CSS is y
+            //if yes create a new folder for it
+            //print where it is located
+    }
+
+}
