4512b0845926274cf607e2b0e4e202dddcbf3c05
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-16 14:00:12 -0400

    skeletone code

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+
+package baseline;
+
+public class Solution44 {
+    //create a counter that is = 0
+    //while loop that runs until counter is not 0
+        //ask for the user input by calling readInput
+        //call search function with the input
+        //if an item was found change counter to 1
+
+
+
+
+
+}
---
+++ exercise44/src/main/java/baseline/productSearch.java
@@ -0,0 +1,29 @@
+package baseline;
+import java.util.Scanner;
+public class productSearch {
+    private static final Scanner in = new Scanner(System.in);
+    String searchTerm ;
+
+    private void readInput(String prompt){
+        //prompts the user for a string
+        //returns the user next line
+    }
+
+    public int search(){
+        //create a new instance of Json
+
+        //use a try thing to open the document
+
+            // parse the data to see if the input is equal to any of the names
+            //done by doing a for loop for terms in the input file
+            //if the input is not there
+                // print a statement saying it is not here and return 0
+            //else get all the data
+            // save name price and quantity
+            // print them out
+            // return 1
+        //use a catch loop so it will run
+
+    }
+
+}
