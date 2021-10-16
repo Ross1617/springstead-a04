8f1989fdfb66c6c04b62dd4a035b1eb10d69c033
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-16 15:36:10 -0400

    skeletone code

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,17 @@
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args) {
+        //creates a new instance of class wordFinder
+
+        //calls fileReader
+
+        //calls outputWriter
+
+    }
+}
---
+++ exercise45/src/main/java/baseline/wordFinder.java
@@ -0,0 +1,26 @@
+package baseline;
+
+
+import java.util.ArrayList;
+
+public class wordFinder {
+    //array list that holds all the words
+
+    public ArrayList<String> readNames(String filename){
+        //opens input file
+        //reads in the file names from the string provided
+        //adds the names to the array list
+        //returns names for testing purposes
+        //close the scanner (I hate sonarLint)
+    }
+    public ArrayList<String> writeNames(String filename){
+        //opens output file
+        //creats new array list
+        // uses try to open the file
+            // goes through every word
+            // if the word is equal to utilize replace it
+            //write the word to the new array list
+        //returns the new array list
+    }
+
+}
