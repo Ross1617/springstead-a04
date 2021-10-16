cb6d93ce5af340c7ee95e8bf71cb77ddcfd5f2d8
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-16 17:14:46 -0400

    skeletone code

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+
+public class Solution46 {
+    public static void main(String[] args) {
+        //creates new instance of wordFrequencyFinder
+
+        //calls readInput
+
+        // calls findFrequency
+
+        //calls displaysOutput
+
+
+    }
+}
---
+++ exercise46/src/main/java/baseline/wordFrequencyFinder.java
@@ -0,0 +1,39 @@
+package baseline;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class wordFrequencyFinder {
+    //create instance variable of array List to hold input
+    //instance variable of the amount of certain words
+    //badger
+    //mushroom
+    //snake
+    public ArrayList<String> readInput(String filename) {
+        //opens input file
+        //reads in the file names from the string provided
+         //adds the names to the array list
+
+        //returns names for testing purposes
+        //close the scanner
+    }
+    public int findFrequency(){
+        //goes throught the array list
+        //increases every time badger shows up
+        //increase everytime mushroom shows up
+        //increases everytime snake shows up
+        //adds up all the number s
+        //returns the number
+    }
+    public void displaysOutput(){
+        //adds the three numbers into an array list
+        //sorts them by greatest to least
+        //prints the name and the amount
+            //prints * for the amount they show up
+
+    }
+
+
+
+}
